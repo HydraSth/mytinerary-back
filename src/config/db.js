00374a3346be8d9996227fa0a1ac5ba35870bb6d
Juan Cruz <juanespasan@gmail.com>
2023-08-18 03:26:37 -0300

    Back sprint-2

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
+require('dotenv').config({path:'./.env'});
 const {connect} = require('mongoose')
 
-const pass='oxa6VBAY3ZKyOjfe';
-const URI=`mongodb+srv://juanespasan:${pass}@mitinerary.bajxpii.mongodb.net/?retryWrites=true&w=majority`;
+const URI=`mongodb+srv://juanespasan:${process.env.mongo_pass}@mitinerary.bajxpii.mongodb.net/?retryWrites=true&w=majority`;
 const connectDb = ()=>connect(URI)
 .then(() => { console.log('Connected to database')})
 .catch((e)=>{console.log(`Error connecting: ${e}`)})
